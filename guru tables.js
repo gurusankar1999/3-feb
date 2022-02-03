c1413baf0482505b3039bb2d3dbb90c56c703e03
gurusankar1999 <98955278+gurusankar1999@users.noreply.github.com>
2022-02-03 18:04:28 +0530

    Add files via upload

---
+++ guru tables.js
@@ -0,0 +1,7 @@
+var a=window.prompt  ( "value");
+var b;
+for (i=1;i<=a;i++)
+{ 
+  b=i*a
+  document.write(i,"*",a,"=",b+"<br>")
+}
---
+++ guru.js
@@ -0,0 +1,24 @@
+function add(a,b){
+  console.log(a+b);
+}
+add(5,3);
+
+function sub(i,k){
+  console.log(i-k);
+}
+sub(8,7);
+
+function mulp(p,r){
+console.log(p*r);
+}
+mulp(5,5);
+
+function div(l,g){
+  console.log(l/g);
+}
+ div(10,5);
+
+function rem(d,s){
+  console.log(d%s);
+}
+rem(20,10);
